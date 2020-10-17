28e437d3aaf26f48dea44ef116cfd2676fda18ab
Pasha Technik <pacha120431@gmail.com>
2020-10-17 21:44:45 +0300

    Add registration screen

--- EnglishApp/ViewController.swift
+++ EnglishApp/ViewController.swift
@@ -10,12 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var LoginBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world")
-        print("Hello Pasha!")
+
+        EmailTextField.addDoneButtonOnKeyboard()
+        PasswordTextField.addDoneButtonOnKeyboard()
     }
+    
 
 
 }
+
+extension UITextField{
+    
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
+    }
+}
+
 
