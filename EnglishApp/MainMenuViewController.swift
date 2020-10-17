6675b76eca7c8696eb878ad33182e8ddf8b1ab80
srgbrs <borisov11092000@gmail.com>
2020-10-17 21:48:25 +0300

    main menu view controller

---
+++ EnglishApp/MainMenuViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    @IBOutlet weak var EngLevelView: UIView!
+    @IBOutlet weak var GreetingLabel: UILabel!
+    @IBOutlet weak var RefreshButtonView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        EngLevelView.layer.cornerRadius = 15;
+        EngLevelView.layer.masksToBounds = true;
+        
+        RefreshButtonView.layer.cornerRadius = 15;
+        RefreshButtonView.layer.masksToBounds = true;
+        
+        //self.view.backgroundColor = UIColor(
+        
+}
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
+    
+
+}
