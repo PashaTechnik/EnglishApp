8fd39f69b0a8e5d212c50e7f90bd7d5138738187
Pasha Technik <pacha120431@gmail.com>
2021-04-21 18:08:22 +0300

    MVVM struct

--- EnglishApp/NetworkManager.swift
+++ EnglishApp/NetworkManager.swift
@@ -7,7 +7,29 @@
 //
 
 import Foundation
+import Firebase
 
 class NetworkManager: NSObject {
     
+     func fetchData(completion: @escaping (User) -> ()){
+        let db = Firestore.firestore()
+        let currentUserId = Auth.auth().currentUser?.uid
+        let user = db.collection("users").whereField("uid", isEqualTo: currentUserId!)
+        user.getDocuments { (query, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in query!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    let firstName = document.data()["firstname"] as! String
+                    let lastName = document.data()["lastname"] as! String
+                    let points = document.data()["points"] as! Int
+                    let dictionary = document.data()["Dictionary"] as! Dictionary<String,String>
+                    let currentUser = User(firstName: firstName, lastName: lastName, email: "", points: points, dictionary: dictionary)
+                    completion(currentUser)
+                }
+            }
+        }
+    }
 }
+
