6a7e02fc0025a8b4e34298d612106d23b121b23c
srgbrs <borisov11092000@gmail.com>
2021-05-22 22:25:04 +0300

    - Tab Bar design added (iPhone 11 custom layout)

--- EnglishApp/MainTabBar.swift
+++ EnglishApp/MainTabBar.swift
@@ -1,11 +1,3 @@
-//
-//  MainTabBar.swift
-//  EnglishApp
-//
-//  Created by Pasha on 26.10.2020.
-//  Copyright © 2020 Pasha. All rights reserved.
-//
-
 import UIKit
 
 class MainTabBar: UITabBarController {
@@ -19,6 +11,24 @@ class MainTabBar: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UITabBar.appearance().barTintColor = UIColor.white
+        UITabBar.appearance().isTranslucent = false
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Montserrat-Medium", size: 10)!], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Montserrat-Medium", size: 10)!], for: .selected)
+        
+        if let items = tabBar.items {
+                    for item in items {
+                        item.setTitleTextAttributes([.foregroundColor: UIColor(red: 55.0/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1)], for: .selected)
+                        item.setTitleTextAttributes([.foregroundColor: UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1)], for: .normal)
+                        item.image?.withTintColor(UIColor(red: 55.0/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+                    }
+                }
+        
+        self.tabBar.unselectedItemTintColor = UIColor(red: 55.0/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1.0)
+        
+
+
 
         DispatchQueue.main.async {
             self.fetchUser()
